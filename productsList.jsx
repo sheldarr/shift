a3e55ada64d711100eec35d902f79bce59d77649
sheldar@wp.pl <sheldar@wp.pl>
2015-10-10 21:20:31 +0200

    Added css-loader.

--- productsList.jsx
+++ productsList.jsx
@@ -4,6 +4,8 @@ var React = require('react');
 var Table = require('elemental').Table;
 var Button = require('elemental').Button;
 
+require.context('./node_modules/elemental/less/', true, /\.less$/);
+
 module.exports = React.createClass({
 	getDefaultProps: function() {
 		return {
