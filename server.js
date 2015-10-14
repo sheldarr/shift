e85dc288e3480471342ae53a91361823383ddf65
sheldar@wp.pl <sheldar@wp.pl>
2015-10-14 18:21:07 +0200

    Added simple express api router.

---
+++ server.js
@@ -0,0 +1,20 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();      
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = 8088;
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'OK' });   
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Shift api server is running on port: ' + port);
