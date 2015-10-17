e35a10463c02e4b081ebc61d3f012c78e074d6b1
sheldar@wp.pl <sheldar@wp.pl>
2015-10-17 12:49:25 +0200

    Updated patient info page.

--- pages/patient.jsx
+++ pages/patient.jsx
@@ -3,11 +3,16 @@
 var React = require('react');
 
 var Button = require('react-bootstrap').Button;
+var Col = require('react-bootstrap').Col;
 var Glyphicon = require('react-bootstrap').Glyphicon;
+var Grid = require('react-bootstrap').Grid;
 var Input = require('react-bootstrap').Input;
 var Panel = require('react-bootstrap').Panel;
 var Request = require('superagent');
+var Row = require('react-bootstrap').Row;
 
+var Enums = require('../api/enums');
+var IndexCalculator = require('../logic/indexCalculator');
 var PatientsService = require('../services/patientsService');
 
 module.exports = React.createClass({
@@ -18,7 +23,9 @@ module.exports = React.createClass({
 	        	name: '',
 	        	weight: 0,
 	        	height: 0,
-	        	age: 0
+	        	age: 0,
+	        	factor: 0,
+	        	sex: Enums.sex.male
 	        }  
 	    };
 	},
@@ -33,15 +40,57 @@ module.exports = React.createClass({
 				alert('Api error ' + error)
 			});
   	},
+	calculateBmi() {
+		return (IndexCalculator.calculateBmi(this.state.patient.weight,
+			this.state.patient.height)).toFixed(2);
+	},
+	calculateBmr() {	
+		return (IndexCalculator.calculateBmr(this.state.patient.weight,
+			this.state.patient.height, this.state.patient.age, 
+			this.state.patient.sex)).toFixed(2);
+	},
+	calculateCpr() {
+		return (IndexCalculator.calculateCpr(this.state.patient.weight,
+			this.state.patient.height, this.state.patient.age,
+			this.state.patient.sex, this.state.patient.factor)).toFixed(2);
+	},
+	getBmiCategory() {
+		return IndexCalculator.getBmiCategory(this.state.patient.weight,
+			this.state.patient.height);
+	},
 	render() {
 		return (
 			<Panel header={<span><Glyphicon glyph="user" /> {this.state.patient.name}</span>}>
-				<Input type="number" label="Weight" addonAfter="kg" readOnly value={this.state.patient.weight} />
-				<Input type="number" label="Height" addonAfter="cm" readOnly value={this.state.patient.height} />
-				<Input type="number" label="Age" addonAfter="years" readOnly value={this.state.patient.age} />
-				<Button bsStyle="primary" href="#/patients">
-    		 		<Glyphicon glyph="arrow-left" /> Back
-		 		</Button>
+				<Row>
+					<Col md={6}>
+						<Panel header="Information">
+							<Input type="number" label="Weight" addonAfter="kg" readOnly value={this.state.patient.weight} />
+							<Input type="number" label="Height" addonAfter="cm" readOnly value={this.state.patient.height} />
+							<Input type="number" label="Age" addonAfter="years" readOnly value={this.state.patient.age} />
+							<Input type="number" label="Factor" readOnly value={this.state.patient.factor} />
+							<div className="input-group">
+		      	  				<label>Gender</label>
+		      	  				<Input type="radio" label="Male" disabled value={Enums.sex.male} checked={this.state.patient.sex == Enums.sex.male} name="sex" />
+		       	 				<Input type="radio" label="Female" disabled value={Enums.sex.female} checked={this.state.patient.sex == Enums.sex.female} name="sex" />
+							</div>
+						</Panel>
+					</Col>
+					<Col md={6}>
+						<Panel header="Status">
+							<Input type="number" label="BMI (Body Mass Index)" readOnly value={this.calculateBmi()} />
+							<Input type="text" label="Category" readOnly value={this.getBmiCategory()} />
+							<Input type="number" label="BMR (Basal Metabolic Rate)" addonAfter="kcal / day" readOnly value={this.calculateBmr()} />
+							<Input type="number" label="CPR (Cosmic Power Regeneration)" addonAfter="kcal / day" readOnly value={this.calculateCpr()} />
+						</Panel>
+					</Col>
+				</Row>
+				<Row>
+					<Col md={12}>
+						<Button bsStyle="primary" href="#/patients">
+	    		 			<Glyphicon glyph="arrow-left" /> Back
+			 			</Button>
+		 			</Col>
+				</Row>
 			</Panel>
 		);
 	}
