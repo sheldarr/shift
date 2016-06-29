8965441e6f83ca73f9fc073307ee2e0a2d7b4038
sheldar@wp.pl <sheldar@wp.pl>
2016-06-29 22:11:46 +0200

    Trying to make it working once again

--- app.jsx
+++ app.jsx
@@ -1,58 +1,48 @@
 'use strict'
 
-import { React } from 'react';
+import React from 'react';
 import { Route, Router } from 'react-router';
-import { ReactDOM } from 'react-dom';
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-// var Router = require('react-router').Router;
-// var Route = require('react-router').Route;
-//
-// var Col = require('react-bootstrap').Col;
-// var Grid = require('react-bootstrap').Grid;
-// var Row = require('react-bootstrap').Row;
-//
-// var Calculator = require('./pages/calculator');
-// var Menu = require('./pages/menu');
-// var NotFound = require('./pages/notFound');
-// var Patients = require('./pages/patients');
-// var Patient = require('./pages/patient');
-// var Products = require('./pages/products');
-//
-// var NavigationBar = require('./components/navigationBar')
-//
-// const App = React.createClass({
-//     render() {
-//         return (
-//             <div>
-//                 {/*<NavigationBar />*/}
-//                 <Grid>
-//                     <Row>
-//                         <Col>
-//                             {this.props.children}
-//                         </Col>
-//                     </Row>
-//                 </Grid>
-//             </div>
-//         );
-//     }
-// });
-//
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('content')
-);
+import { Col, Grid, Row } from 'react-bootstrap';
 
-// ReactDOM.render((
-//     <Router>
-//         <Route path="/" component={App}>
-//             <Route path="/patients" component={Patients} />
-//             <Route path="/patient/:patientId" component={Patient} />
-//             <Route path="/patient/:patientId/menu/:menuId" component={Menu} />
-//             <Route path="/products" component={Products} />
-//             <Route path="/calculator" component={Calculator} />
-//             <Route path="*" component={NotFound}/>
-//         </Route>
-//     </Router>
-// ), document.getElementById('content'));
+// import Calculator from './pages/calculator.jsx';
+// var Menu = require('./pages/menu.jsx');
+// var NotFound = require('./pages/notFound.jsx');
+// var Patients = require('./pages/patients.jsx');
+// var Patient = require('./pages/patient.jsx');
+// var Products = require('./pages/products.jsx');
+
+console.log(React);
+
+var NavigationBar = require('./components/navigationBar.jsx')
+
+const App = React.createClass({
+    render() {
+        return (
+            <div>
+                <NavigationBar />
+                <Grid>
+                    <Row>
+                        <Col>
+                            {this.props.children}
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render((
+    <Router>
+        <Route path="/" component={App}>
+            {/*<Route path="/patients" component={Patients} />
+            <Route path="/patient/:patientId" component={Patient} />
+            <Route path="/patient/:patientId/menu/:menuId" component={Menu} />
+            <Route path="/products" component={Products} />
+            <Route path="/calculator" component={Calculator} />
+            <Route path="*" component={NotFound}/>*/}
+        </Route>
+    </Router>
+), document.getElementById('content'));
