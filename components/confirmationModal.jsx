b2eb18c4a7232f9ac1b8f4c8078ffc416ac7725f
sheldar@wp.pl <sheldar@wp.pl>
2016-07-06 21:31:27 +0200

    Confirmation modal refactored

--- components/confirmationModal.jsx
+++ components/confirmationModal.jsx
@@ -1,44 +1,47 @@
-'use strict'
+'use strict';
 
-var React = require('react');
+import {Button, Glyphicon, Modal} from 'react-bootstrap';
 
-var Button = require('react-bootstrap').Button;
-var Glyphicon = require('react-bootstrap').Glyphicon;
-var Modal = require('react-bootstrap').Modal;
+import React from 'react';
 
-module.exports = React.createClass({
-	propTypes: {
-	    title: React.PropTypes.string.isRequired,
-     	body: React.PropTypes.string,
-		show: React.PropTypes.bool.isRequired,
-		onHide: React.PropTypes.func.isRequired
-	},
-	confirm() {
-		this.props.onHide(true);
-	},
-	cancel() {
-		this.props.onHide(false);
-	},
-	render() {
-		return (
-			<Modal show={this.props.show} onHide={this.props.onHide}>
-				<Modal.Header closeButton>
-					<Modal.Title>{this.props.title}</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					{this.props.body}
-				</Modal.Body>
-				<Modal.Footer>
-					<Button bsStyle="success" 
-						onClick={this.confirm}>
-						<Glyphicon glyph="ok"/> Confirm
-					</Button>
-					<Button bsStyle="danger" style={{marginLeft: 20}} 
-						onClick={this.cancel}>
-						<Glyphicon glyph="remove"/> Cancel
-					</Button>
-				</Modal.Footer>
-			</Modal>
-		);
-	}
-})
+const ConfirmationModal = React.createClass({
+    propTypes: {
+        body: React.PropTypes.string,
+        onHide: React.PropTypes.func.isRequired,
+        show: React.PropTypes.bool.isRequired,
+        title: React.PropTypes.string.isRequired
+    },
+
+    confirm () {
+        this.props.onHide(true);
+    },
+
+    cancel () {
+        this.props.onHide(false);
+    },
+
+    render () {
+        return (
+            <Modal onHide={this.props.onHide} show={this.props.show}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{this.props.title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {this.props.body}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button bsStyle="success" onClick={this.confirm}>
+                        <Glyphicon glyph="ok"/>
+                        {'Confirm'}
+                    </Button>
+                    <Button bsStyle="danger" onClick={this.cancel} style={{marginLeft: 20}}>
+                        <Glyphicon glyph="remove"/>
+                        {'Cancel'}
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+});
+
+export default ConfirmationModal;
