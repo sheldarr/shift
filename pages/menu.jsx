99c1f2639e51ea27ebd32181e92dbfd3e30ab7a2
sheldar@wp.pl <sheldar@wp.pl>
2016-07-07 18:37:38 +0200

    Refctor of menu.jsx

--- pages/menu.jsx
+++ pages/menu.jsx
@@ -1,68 +1,56 @@
-'use strict'
+'use strict';
 
-var _ = require('lodash');
-var React = require('react');
-var Request = require('superagent');
+import {Glyphicon, Panel} from 'react-bootstrap';
 
-var Button = require('react-bootstrap').Button;
-var Glyphicon = require('react-bootstrap').Glyphicon;
-var Input = require('react-bootstrap').Input
-var Panel = require('react-bootstrap').Panel;
-
-var MenuService = require('../services/menuService');
-var PatientService = require('../services/patientsService');
-
-var IndexCalculator = require('../logic/indexCalculator');
+import MenuService from '../services/menuService';
+import PatientService from '../services/patientsService';
+import React from 'react';
 
 module.exports = React.createClass({
-	getInitialState() {
-	    return {
-	    	patient: {
-	    		id: 0,
-	    		name: '',
-	    		weight: 0,
-	    		height: 0,
-	    		age: 0,
-	    		factor: 0,
-	    		sex: 0
-	    	},
-    	  	menu: {
-    	  		id: 0,
-    	  		name: '',
-    	  		days: 0,
-    	  		startDate: 0,
-    	  	}
-	    };
-	},
-	componentDidMount() {
-	    this.refreshMenu();
-  	},
-    refreshMenu() {
-    	PatientService.getById(this.props.params.patientId)
-			.then(response => {
-				this.setState({
-		   			patient: response,
-		   		});
-			})
-			.catch(error => { 
-				alert('Api error ' + error)
-			});
-    	MenuService.getById(this.props.params.patientId, this.props.params.menuId)
-			.then(response => {
-				this.setState({
-		   			menu: response,
-		   		});
-			})
-			.catch(error => { 
-				alert('Api error ' + error)
-			});
+    propTypes: {
+        params: React.PropTypes.array.isRequired
     },
-	render() {
-		return(
-			<Panel header={<span><Glyphicon glyph="list" /> {this.state.menu.name}</span>}>
-				<Panel header="Settings">
-				</Panel>
-			</Panel>
-		);
-	}
-})
+
+    getInitialState () {
+        return {
+            patient: {
+                id: 0,
+                name: '',
+                weight: 0,
+                height: 0,
+                age: 0,
+                factor: 0,
+                sex: 0
+            },
+            menu: {
+                id: 0,
+                name: '',
+                days: 0,
+                startDate: 0
+            }
+        };
+    },
+
+    componentDidMount () {
+        this.refreshMenu();
+    },
+
+    refreshMenu () {
+        PatientService.getById(this.props.params.patientId).then((response) => {
+            this.setState({patient: response});
+        }).catch((error) => {
+            alert(`Api error ${error}`);
+        });
+        MenuService.getById(this.props.params.patientId, this.props.params.menuId).then((response) => {
+            this.setState({menu: response});
+        }).catch((error) => {
+            alert(`Api error ${error}`);
+        });
+    },
+
+    render () {
+        return (
+            <Panel header={<span><Glyphicon glyph="list"/>{this.state.menu.name} </span>}/>
+        );
+    }
+});
