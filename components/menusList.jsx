39268311c32f712d291c00fdeddfa218a2576b2a
bartosz.baczek <bartosz.baczek@gmail.com>
2016-07-10 13:27:05 +0200

    Fixed linter issues in menusList.jsx

--- components/menusList.jsx
+++ components/menusList.jsx
@@ -1,55 +1,54 @@
-'use string'
+'use string';
 
-var React = require('react');
+import {Button, Glyphicon, Table} from 'react-bootstrap';
 
-var Button = require('react-bootstrap').Button;
-var Glyphicon = require('react-bootstrap').Glyphicon;
-var Table = require('react-bootstrap').Table;
+import React from 'react';
 
 module.exports = React.createClass({
-	propTypes: {
-		menus: React.PropTypes.array.isRequired,
-		patientId: React.PropTypes.number.isRequired
-	},
-	render() {
-		return (
-			<Table striped hover>
-				<thead>
-					<tr>
-						<td>Id</td>
-						<td>Name</td>
-						<td>Days</td>
-						<td>Start date</td>
-						<td></td>
-					</tr>
-				</thead>
-				<tbody>
-					{this.props.menus.map(menu => 
-						<tr key={menu.id}>
-							<td>
-								{menu.id}
-							</td>
-							<td>
-								{menu.name}
-							</td>
-							<td>
-								{menu.days}
-							</td>
-							<td>
-								{new Date(menu.startDate).toDateString()}
-							</td>
-							<td>
-								<div className="pull-right">
-									 <Button bsStyle="primary" style={{marginRight: 10}} 
-									 	href={`#/patient/${this.props.patientId}/menu/${menu.id}`}>
-									 	<Glyphicon glyph="pencil" /> Edit
-								 	</Button>
-							 	</div>
-							</td>
-						</tr>
-					)}
-				</tbody>
-			</Table>
+    propTypes: {
+        menus: React.PropTypes.array.isRequired,
+        patientId: React.PropTypes.number.isRequired
+    },
+
+    render () {
+        return (
+            <Table hover striped>
+                <thead>
+                    <tr>
+                        <td>{'Id'}</td>
+                        <td>{'Name'}</td>
+                        <td>{'Days'}</td>
+                        <td>{'Start date'}</td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.props.menus.map((menu) =>
+                    <tr key={menu.id}>
+                        <td>
+                            {menu.id}
+                        </td>
+                        <td>
+							{menu.name}
+                        </td>
+                        <td>
+                            {menu.days}
+                        </td>
+                        <td>
+							{new Date(menu.startDate).toDateString()}
+                        </td>
+                        <td>
+                            <div className="pull-right">
+                                <Button bsStyle="primary" href={`#/patient/${this.props.patientId}/menu/${menu.id}`}>
+                                    style={{marginRight: 10}}
+                                    <Glyphicon glyph="pencil" /> {'Edit'}
+                                </Button>
+                            </div>
+                        </td>
+                    </tr>
+                )}
+                </tbody>
+            </Table>
 		);
-	}
+    }
 });
