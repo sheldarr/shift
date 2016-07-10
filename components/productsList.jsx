06d3946363eb4be079585b840ddfced6db898033
sheldar@wp.pl <sheldar@wp.pl>
2016-07-10 14:11:34 +0200

    Experimenting with reactable

--- components/productsList.jsx
+++ components/productsList.jsx
@@ -1,7 +1,7 @@
 'use string';
 
 import React from 'react';
-import {Table} from 'react-bootstrap';
+import {Table} from 'reactable';
 
 module.exports = React.createClass({
     propTypes: {
@@ -9,77 +9,44 @@ module.exports = React.createClass({
     },
 
     render () {
+        const columns = [
+            {
+                key: 'id',
+                label: 'Id'
+            },
+            {
+                key: 'name',
+                label: 'Name'
+            },
+            {
+                key: 'energyValue',
+                label: 'Energy Value [kcal]'
+            },
+            {
+                key: 'protein',
+                label: 'Protein [g]'
+            },
+            {
+                key: 'fat',
+                label: 'Fat [g]'
+            },
+            {
+                key: 'carbohydrates',
+                label: 'Carbohydrates [g]'
+            },
+            {
+                key: 'fiber',
+                label: 'Fiber [g]'
+            }
+        ];
+
         return (
-            <Table hover striped>
-                <thead>
-                    <tr>
-                        <td>{'Name'}</td>
-                        <td>{'Energy Value [kcal]'}</td>
-                        <td>{'Protein [g]'}</td>
-                        <td>{'Fat [g]'}</td>
-                        <td>{'Carbohydrates [g]'}</td>
-                        <td>{'Fiber [g]'}</td>
-                        <td>{'Sodium [mg]'}</td>
-                        <td>{'Potasium [mg]'}</td>
-                        <td>{'Calcium [mg]'}</td>
-                        <td>{'Phosphorus [mg]'}</td>
-                        <td>{'Iron [mg]'}</td>
-                        <td>{'Magnesium [mg]'}</td>
-                        <td>{'Vitamin A [μg]'}</td>
-                        <td>{'Beta Carotene [μg]'}</td>
-                        <td>{'Vitamin E [mg]'}</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.products.map((product) => <tr key={product.id}>
-                        <td>
-                            {product.name}
-                        </td>
-                        <td>
-                            {product.energyValue}
-                        </td>
-                        <td>
-                            {product.protein}
-                        </td>
-                        <td>
-                            {product.fat}
-                        </td>
-                        <td>
-                            {product.carbohydrates}
-                        </td>
-                        <td>
-                            {product.fiber}
-                        </td>
-                        <td>
-                            {product.sodium}
-                        </td>
-                        <td>
-                            {product.potassium}
-                        </td>
-                        <td>
-                            {product.calcium}
-                        </td>
-                        <td>
-                            {product.phosphorus}
-                        </td>
-                        <td>
-                            {product.iron}
-                        </td>
-                        <td>
-                            {product.magnesium}
-                        </td>
-                        <td>
-                            {product.vitaminA}
-                        </td>
-                        <td>
-                            {product.betaCarotene}
-                        </td>
-                        <td>
-                            {product.vitaminE}
-                        </td>
-                    </tr>)}
-                </tbody>
-            </Table>
+            <div className="table-responsive">
+                <Table className="table table-condensed  table-hover table-striped" columns={columns} currentPage={1}
+                    data={this.props.products} filterable={['id', 'name']} itemsPerPage={16}
+                    noDataText="No records." pageButtonLimit={10} sortable
+                />
+            </div>
         );
     }
 });
