8262db15123bc1e066bf93eb6cac5334bed1adde
sheldar@wp.pl <sheldar@wp.pl>
2016-07-12 17:16:01 +0200

    Removed not needed response header

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ app.use(i18n.init);
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
     next();
 });
 
