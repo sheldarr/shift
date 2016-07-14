cc601e57206bdd442e5b7b550030880f4069f1ad
sheldar@wp.pl <sheldar@wp.pl>
2016-07-14 21:43:46 +0200

    Added auth endpoint for user data

--- src/backend/routers/authRouter.js
+++ src/backend/routers/authRouter.js
@@ -5,16 +5,24 @@ const winston = require('winston');
 
 const authRouter = new express.Router();
 
+authRouter.get('/auth', (request, response) => {
+    if (request.user) {
+        response.json(request.user);
+    }
+
+    response.sendStatus(401);
+});
+
 authRouter.get('/auth/basic', passport.authenticate('basic'), (request, response) => {
     winston.info(`Login ${JSON.stringify(request.user)}`);
-    response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    response.sendFile(path.resolve(__dirname, '../../..', 'public', 'index.html'));
 });
 
 authRouter.get('/auth/logout', (request, response) => {
     winston.info(`Logout ${request.user}`);
 
     request.logOut();
-    response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    response.sendFile(path.resolve(__dirname, '../../..', 'public', 'index.html'));
 });
 
 module.exports = authRouter;
