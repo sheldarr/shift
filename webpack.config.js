78b69a77027c5b9374a5472232027a070b52841b
sheldar <kewin.polok@gmail.com>
2017-01-16 18:36:27 +0100

    Bootstrap is now downloaded during webpack build process

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,23 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const DefinePlugin = require('webpack').DefinePlugin;
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
+const https = require('https');
+const fs = require('fs-extra');
+const path = require('path');
+
+const bootstrapPath = path.normalize('./build/public/css/bootstrap.min.css');
+const file = fs.createWriteStream(bootstrapPath);
+
+https.get('https://bootswatch.com/paper/bootstrap.min.css', (response) => {
+    response.pipe(file);
+
+    file.on('finish', function() {
+        file.close();
+    });
+}).on('error', function() {
+    fs.unlink(bootstrapPath);
+});
+
 module.exports = {
     entry: './app.jsx',
     output: {
@@ -27,9 +44,9 @@ module.exports = {
                 to: './public/css'
             }
         ]),
-        new ProgressBarPlugin(),
         new DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-        })
+        }),
+        new ProgressBarPlugin()
     ]
 };
