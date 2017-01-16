77da56d16bf95e51f55f004e803bcd4048c11481
sheldar <kewin.polok@gmail.com>
2017-01-16 20:48:57 +0100

    Initializer can now work from CLI or be imported

--- utils/initializer.js
+++ utils/initializer.js
@@ -2,9 +2,17 @@ const fs = require('fs-extra');
 const path = require('path');
 const winston = require('winston');
 
-fs.copy(path.normalize('./initial-data'), path.normalize('./data'), function(error) {
-    if (error) {
-        winston.error(error);
-        process.exit(1);
-    }
-});
+const run = () => {
+    fs.copy(path.normalize('./initial-data'), path.normalize('./data'), function(error) {
+        if (error) {
+            winston.error(error);
+        }
+    });
+};
+
+if(module.parent) {
+    module.exports = {run};
+    process.exit(0);
+}
+
+run();
