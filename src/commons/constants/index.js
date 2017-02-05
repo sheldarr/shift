78f499d86e2a21d9711ac065ce67427d9786fcb5
sheldar <kewin.polok@gmail.com>
2017-02-05 15:36:10 +0100

    Added si constants for liter and gram

--- src/commons/constants/index.js
+++ src/commons/constants/index.js
@@ -4,6 +4,32 @@ const constants = {
     sex: {
         male: 'male',
         female: 'female'
+    },
+    units: {
+        si: {
+            'g': {
+                modifier: 1
+            },
+            'mg': {
+                base: 'g',
+                modifier: 0.001
+            },
+            'dag': {
+                base: 'g',
+                modifier: 0.01
+            },
+            'kg': {
+                base: 'g',
+                modifier: 1
+            },
+            'l': {
+                modifier: 1
+            },
+            'ml': {
+                base: 'l',
+                modifier: 0.001
+            }
+        }
     }
 };
 
